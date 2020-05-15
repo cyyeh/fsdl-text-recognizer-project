ef514167fda3ef0cf026ba974002780d763afb66
cyyeh <chihyu.jimmy.yeh@gmail.com>
2020-05-15 10:17:45 +0000

    update customed work

--- final/text_recognizer/util.py
+++ final/text_recognizer/util.py
@@ -1,13 +1,37 @@
 """
 Utility functions for text_recognizer module.
 """
-from typing import Union
 from pathlib import Path
+from typing import Union
+from urllib.request import urlopen, urlretrieve
 import hashlib
-from urllib.request import urlretrieve
 
+import cv2
 from tqdm import tqdm
 
+def read_image(image_uri: Union[Path, str], grayscale=False) -> np.array:
+    """Read image_uri."""
+    def read_iamge_from_filename(image_filename, imread_flag):
+        return cv2.imread(str(image_filename), imread_flag)
+    
+    def read_iamge_from_url(image_url, imread_flag):
+        url_response = urlopen(str(image_url))
+        img_array = np.array(bytearray(url_response.read()), dtype=np.uint8)
+        return cv2.imdecode(img_array, imread_flag)
+    
+    imread_flag = cv2.IMREAD_GRAYSCALE if grayscale else cv2.IMREAD_COLORE
+    local_file = os.path.exists(image_uri)
+    
+    try:
+        img = None
+        if local_file:
+            img = read_image_from_filename(image_uri, imread_flag)
+        else:
+            img = read_image_from_url(image_uri, imread_flag)
+        assert img is not None
+    except Exception as e:
+        raise ValueError(f'Could not load iamge at {image_uri}: {e}')
+    return img
 
 def compute_sha256(filename: Union[Path, str]):
     """Return SHA256 checksum of a file."""
